8619e688a24ce9b5f6b19f4d2116ce236cacfb9e
toledoneto <toledo.neto@outlook.com>
2018-07-10 15:59:17 -0300

    W03 - Prototype

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -6,6 +6,8 @@ class WhatsAppController
 
 		console.log("WhatsAppController OK!");
 
+		this.elementsPrototype();
+
 		// metodo para carregar os 75 ids principais do projeto
 		// e transformar em camelCase cada um deles para uso posterior
 		this.loadElements();
@@ -24,6 +26,83 @@ class WhatsAppController
 			this.el[Format.getCamelCase(element.id)] = element;
 
 		});
+
+	}
+
+	elementsPrototype()
+	{
+
+		// não se usa uma arrow function para fechar o escopo da função
+		// qquer elemento que tiver o '.hide' será ocultado
+		Element.prototype.hide = function(argument)
+		{
+			this.style.display = 'none';
+			return this;
+		};
+
+		Element.prototype.show = function(argument)
+		{
+			this.style.display = 'block';
+			return this;
+		};
+
+		Element.prototype.toggle = function(argument)
+		{
+			this.style.display = (this.style.display === 'none') ? 'block' : 'none';
+			return this;
+		};
+
+		// função para múltiplos eventos. Eles serão enviados no events como string separados por ' '...
+		Element.prototype.on = function(events, fn)
+		{
+			// ...serapamos por esse ' ', transformando num array, e aplicamos um forEach para cada
+			// evento encontrado
+			events.split(' ').forEach(event => {
+				
+				// arrow func não muda escopo, ou seja, o this aqui se refere ao elemento do prototype
+				this.addEventListener(event, fn);
+			});
+
+			return this;
+
+		};
+
+		Element.prototype.css = function(styles)
+		{
+			
+			for (let name in styles) 
+			{
+				
+				this.style[name] = styles[name];
+
+			}
+
+			return this;
+
+		};
+
+		Element.prototype.addClass = function(name)
+		{
+			this.classList.add(name);
+			return this;
+		};
+
+		Element.prototype.removeClass = function(name)
+		{
+			this.classList.remove(name);
+			return this;
+		};
+
+		Element.prototype.toggleClass = function(name)
+		{
+			this.classList.toggle(name);
+			return this;
+		};
+
+		Element.prototype.hasClass = function(name)
+		{
+			return this.classList.contains(name);
+		};
 
 	}
 

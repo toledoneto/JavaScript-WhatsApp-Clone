bdc8d69e8e7b3ec522cc25462896b47e6032cd48
toledoneto <toledo.neto@outlook.com>
2018-07-11 18:01:27 -0300

    W08 - Eventos de gravação do microfone e timer de gravação

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -114,27 +114,93 @@ class WhatsAppController
 
 		this.el.btnAttachPhoto.on('click', e=>{
 
-			console.log('photo');
+			this.el.inputPhoto.click();
+
+		});
+
+		this.el.inputPhoto.on('change', e=>{
+
+			console.log(this.el.inputPhoto.files);
+
+			// como temos uma coleção, precisamos usar o spread para transformar em array
+			[...this.el.inputPhoto.files].forEach(file=>{
+
+				console.log(file);
+
+			});
 
 		});
 
 		this.el.btnAttachCamera.on('click', e=>{
 
-			console.log('camera');
+			this.closeAllMainPanel();
+			this.el.panelCamera.addClass('open');
+			this.el.panelCamera.css({
+
+				'height':'calc(100% - 120px)'
+
+			});
+
+		});
+
+		this.el.btnClosePanelCamera.on('click', e=>{
+
+			this.el.panelCamera.removeClass('open');
+			this.el.panelMessagesContainer.show();
+
+		});
+
+		this.el.btnTakePicture.on('click', e=>{
+
+			console.log('take picture');
 
 		});
 
 		this.el.btnAttachDocument.on('click', e=>{
 
-			console.log('Document');
+			this.closeAllMainPanel();
+			this.el.panelDocumentPreview.addClass('open');
+			this.el.panelDocumentPreview.css({
+
+				'height':'calc(100% - 120px)'
+
+			});
+
+		});
+
+		this.el.btnClosePanelDocumentPreview.on('click', e=>{
+
+			this.closeAllMainPanel();
+			this.el.panelMessagesContainer.show();
+
+		});
+
+		this.el.btnSendDocument.on('click', e=>{
+
+			console.log('send document');
 
 		});
 
 		this.el.btnAttachContact.on('click', e=>{
 
-			console.log('Contact');
+			this.el.modalContacts.show();
 
 		});
+
+		this.el.btnCloseModalContacts.on('click', e=>{
+
+			this.el.modalContacts.hide();
+
+		});
+
+	}
+
+	closeAllMainPanel()
+	{
+
+		this.el.panelMessagesContainer.hide();
+		this.el.panelDocumentPreview.removeClass('open');
+		this.el.panelCamera.removeClass('open');
 
 	}
 

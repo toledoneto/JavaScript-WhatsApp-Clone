6290dd3e82e9f220fba9671f2549b5f5e8e4dd32
toledoneto <toledo.neto@outlook.com>
2018-08-09 10:59:32 -0300

    W29 - Atualizando Dados do perfil do Usuário

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -148,7 +148,15 @@ export class WhatsAppController
 
 		this.el.btnSavePanelEditProfile.on('click', e=>{
 
-			console.log(this.el.inputNamePanelEditProfile.innerHTML);
+			this.el.btnSavePanelEditProfile.disabled = true;
+
+			this._user.name = this.el.inputNamePanelEditProfile.innerHTML;
+
+			this._user.save().then(() => {
+
+				this.el.btnSavePanelEditProfile.disabled = false;
+
+			});
 
 		});
 

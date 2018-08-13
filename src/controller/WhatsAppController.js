8121db02497c4bf4aa474c649f5fd7c889cd0726
toledoneto <toledo.neto@outlook.com>
2018-08-13 08:48:27 -0300

    W41 - Enviando foto como anexo

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -537,7 +537,61 @@ export class WhatsAppController
 
 		this.el.btnSendPicture.on('click', e=>{
 
-			
+			this.el.btnSendPicture.disabled = true;
+
+			// encontrando o mime type e o base64 da foto da cam com uso de 
+			// expressões regulares
+			let regex = /^data:(.+);base64,(.*)$/;
+			let result = this.el.pictureCamera.src.match(regex);
+			let mimeType = result[1];
+			let ext = mimeType.split('/')[1];
+			let filename = `camera${Date.now()}.${ext}`;
+
+			// a foto da cam fica espelhada quando enviada para a tela
+			// para contornar esse problema, usaremos o canvas para rotacionar
+			let picture = new Image();
+			picture.src = this.el.pictureCamera.src;
+			picture.onload = e => {
+
+				let canvas = document.createElement('canvas');
+				let context = canvas.getContext('2d');
+
+				canvas.width = picture.width;
+				canvas.height = picture.height;
+
+				// transladamos a ft na horizontal uma distancia = ao tam da ft
+				// para que, ao rotacionarmos 180º, ela volte à posição 0, mas
+				// com a orientação correta
+				context.translate(picture.width, 0);
+				// agora fazemos a rotação em cima do eixo y da ft
+				context.scale(-1, 1);
+
+				context.drawImage(picture, 0, 0, canvas.width, canvas.height);
+
+
+
+				fetch(canvas.toDataURL(mimeType))
+				.then(res => { return res.arrayBuffer(); })
+				.then(buffer =>{ return new File([buffer], filename, { type: mimeType });})
+				.then(file => {
+
+					Message.sendImage(this._contactActive.chatId, this._user.email, file);
+
+					this.el.btnSendPicture.disabled = false;
+
+					this.closeAllMainPanel();
+					this._camera.stop();
+					this.el.btnReshootPanelCamera.hide();
+					this.el.pictureCamera.hide();
+					this.el.videoCamera.show();
+					this.el.containerSendPicture.hide();
+					this.el.containerTakePicture.show();
+					this.el.panelMessagesContainer.show();
+
+				});
+
+
+			}
 
 		});
 
